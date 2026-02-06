52b2c488710b18e9e04828ea41ffee01758b0da0
Appropriate32 <frostghost32@gmail.com>
2026-02-06 19:09:27 +0500

    Implemenented the Nav Bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import "./styles.css";
+import logoImg from "./images/logo.png";
 
-function product(a, b) {
-  return a * b;
-}
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-links");
+const icon = hamburger.querySelector("i");
+const logoElement = document.querySelector("#nav-logo");
 
-export default product;
+logoElement.src = logoImg;
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+  if (menu.classList.contains("active")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+  } else {
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+  }
+});
+
+menu.addEventListener("click", (e) => {
+  if (e.target instanceof HTMLAnchorElement) {
+    if (!e.target.classList.contains("active-nav")) {
+      const activeNav = menu.querySelector(".active-nav");
+      activeNav.classList.remove("active-nav");
+      e.target.classList.add("active-nav");
+    }
+  }
+});
--- src/index.test.js
+++
@@ -1,5 +0,0 @@
-import product from "./index.js";
-
-test("multiplies 2 * 4 to equal 8", () => {
-  expect(product(2, 4)).toBe(8);
-});
