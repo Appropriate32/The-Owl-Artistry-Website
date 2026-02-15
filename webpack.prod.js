fa262f0065c6fdd8da6208205f83fe7c074b11cc
Appropriate32 <frostghost32@gmail.com>
2026-02-15 13:31:54 +0500

    installed css minify plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "[name].css" })],
 });
