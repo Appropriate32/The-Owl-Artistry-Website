4276569a7bdf50708e003e2e7a8ba6a9d2ef3de2
Appropriate32 <frostghost32@gmail.com>
2026-02-18 20:10:55 +0500

    Implemented products cards layout

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import logoImg from "./images/logo.png";
 
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".nav-links");
-const icon = hamburger.querySelector("i");
 const logoElement = document.querySelector("#nav-logo");
 const faviconLink = document.querySelector("#favicon");
 const homeOwl = document.querySelector(".hero-owl");
@@ -21,34 +20,40 @@ if (homeOwl) {
   homeOwl.src = logoImg;
 }
 
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
+if (hamburger && menu) {
+  const icon = hamburger.querySelector("i");
 
-  if (menu.classList.contains("active")) {
-    icon.classList.remove("fa-bars");
-    icon.classList.add("fa-times");
-  } else {
-    icon.classList.remove("fa-times");
-    icon.classList.add("fa-bars");
-  }
-});
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
 
-filterButtons.addEventListener("click", (e) => {
-  if (e.target instanceof HTMLButtonElement) {
-    if (!e.target.classList.contains("active-btn")) {
-      const activeBtn = filterButtons.querySelector(".active-btn");
-      activeBtn.classList.remove("active-btn");
-      e.target.classList.add("active-btn");
+    if (menu.classList.contains("active")) {
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-times");
+    } else {
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-bars");
     }
-  }
-});
+  });
 
-menu.addEventListener("click", (e) => {
-  if (e.target instanceof HTMLAnchorElement) {
-    if (!e.target.classList.contains("active-nav")) {
-      const activeNav = menu.querySelector(".active-nav");
-      activeNav.classList.remove("active-nav");
-      e.target.classList.add("active-nav");
+  menu.addEventListener("click", (e) => {
+    if (e.target instanceof HTMLAnchorElement) {
+      if (!e.target.classList.contains("active-nav")) {
+        const activeNav = menu.querySelector(".active-nav");
+        activeNav.classList.remove("active-nav");
+        e.target.classList.add("active-nav");
+      }
     }
-  }
-});
+  });
+}
+
+if (filterButtons) {
+  filterButtons.addEventListener("click", (e) => {
+    if (e.target instanceof HTMLButtonElement) {
+      if (!e.target.classList.contains("active-btn")) {
+        const activeBtn = filterButtons.querySelector(".active-btn");
+        activeBtn.classList.remove("active-btn");
+        e.target.classList.add("active-btn");
+      }
+    }
+  });
+}
